045f49bdfbee7655484251b49bc29b8f08dff21c
Michele De Simone <michele.desimone@pagopa.it>
2024-04-09 10:28:22 +0200

    feat: upsert connector config during registration

--- register-connector.sh
+++ register-connector.sh
@@ -1,39 +1,98 @@
 #!/bin/bash
 
-CONNECTOR_FILE="$1"
-URL="http://localhost:8083/connectors/"
+set -euo pipefail
 
-echo "Waiting for Kafka Connect startup..."
+CONNECTOR_NAME="$(jq -r '.name' $CONNECTOR_CONFIG_PATH)"
+BASE_URL="http://localhost:8083/connectors/"
+STATUS_URL="$BASE_URL/$CONNECTOR_NAME/status"
+CONFIG_URL="$BASE_URL/$CONNECTOR_NAME/config"
+CURRENT_IP="$(ip -4 -br addr show eth0 | awk '{print $3}' | sed -E 's/\/[0-9]+//')"
 
-TIMEOUT_SEC=120
-start_time="$(date -u +%s)"
+function wait_for_kafka_connect() {
+  TIMEOUT_SEC=120
+  start_time="$(date -u +%s)"
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $URL)" != "200" ]]; do
-  current_time="$(date -u +%s)"
-  elapsed_seconds=$(($current_time-$start_time))
+  while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $BASE_URL)" != "200" ]]; do
+    current_time="$(date -u +%s)"
+    elapsed_seconds=$(($current_time-$start_time))
 
-  if [ $elapsed_seconds -gt $TIMEOUT_SEC ]; then
-    echo "Timeout while waiting for Kafka Connect startup"
+    if [ $elapsed_seconds -gt $TIMEOUT_SEC ]; then
+      echo "Timeout while waiting for Kafka Connect startup" >&2
+      exit 1
+    fi
+
+    sleep 1
+  done
+}
+
+function check_connector_worker() {
+  connector_status_response="$(curl -s -w "\n%{http_code}\n" -H "Accept:application/json" "$STATUS_URL")"
+  connector_status_code="$(tail -n1 <<< "$connector_status_response")"
+  connector_status_body="$(sed '$ d' <<< "$connector_status_response")"
+
+  if [ "$connector_status_code" -eq "200" ]; then
+    connector_worker_ip="$(echo "$connector_status_body" | jq -r '.connector.worker_id')"
+
+    # this worker will be idle
+    if [ ! "$CURRENT_IP" = "$connector_worker_ip" ]; then 
+      echo "The connector task has already been assigned to another worker."
+      exit 0
+    fi
+  fi
+}
+
+function upsert_connector() {
+  body_path="$1"
+
+  upsert_response=$(curl -s -w "\n%{http_code}\n" 
+                    -X PUT -H "Accept:application/json" -H "Content-Type:application/json" 
+                    -d "@$body_path" "$CONFIG_URL")
+
+  echo "$upsert_response"
+}
+
+function register_connector() {
+  UNWRAPPED_CONNECTOR_CONFIG_PATH="${CONNECTOR_CONFIG_PATH%.json}-unwrapped.json"
+  jq '.config' "$CONNECTOR_CONFIG_PATH" > "$UNWRAPPED_CONNECTOR_CONFIG_PATH"
+
+  upsert_response="$(upsert_connector "$UNWRAPPED_CONNECTOR_CONFIG_PATH")"
+  upsert_status_code="$(tail -n1 <<< "$upsert_response")"
+
+  REBALANCE_TIMEOUT_SEC=60
+  start_time="$(date -u +%s)"
+
+  while [[ "$upsert_status_code" = "409" ]]; do
+    echo "Connector task rebalancing in progress, retrying..."
+    sleep 2
+
+    current_time="$(date -u +%s)"
+    elapsed_seconds=$(($current_time-$start_time))
+
+    if [ $elapsed_seconds -gt $REBALANCE_TIMEOUT_SEC ]; then
+      echo "Timeout while waiting for connector task rebalance." >&2
+      exit 1
+    fi
+
+    upsert_response="$(upsert_connector "$UNWRAPPED_CONNECTOR_CONFIG_PATH")"
+    upsert_status_code="$(tail -n1 <<< "$upsert_response")"
+  done;
+
+  
+  if [ "$upsert_status_code" -eq "201" ]; then
+    echo "Connector registered successfully."
+  elif [ "$upsert_status_code" -eq "200" ]; then
+    echo "Connector was already registered. Configuration updated."
+  else
+    echo "Error response while registering connector:\n${upsert_response}\n"
     exit 1
   fi
+}
 
-  sleep 1
-done
+echo "Waiting for Kafka Connect startup..."
+wait_for_kafka_connect
 
-echo "Registering connector $CONNECTOR_FILE..."
+echo "Checking connector worker..."
+check_connector_worker
 
-connect_response=$(curl -s -w "\n%{http_code}\n" -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d "@$CONNECTOR_FILE" "$URL")
-
-status_code=$(tail -n1 <<< "$connect_response")
-body=$(sed '$ d' <<< "$connect_response")
-
-echo $body
-
-if [ "$status_code" -eq "201" ]; then
-  echo "Connector registered successfully"
-elif [ "$status_code" -eq "409" ]; then
-  echo "Connector already registered."
-else
-  echo "Error response from Kafka Connect REST API"
-  exit 1
-fi
+echo "Registering connector $UNWRAPPED_CONNECTOR_CONFIG_PATH..."
+upsert_connector
